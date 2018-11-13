c10c992c6abb82d7ace64ccfc83b95d4c4014db6
michaelbabyn <michael@plot.ly>
2018-11-13 15:03:55 -0500

    initial

---
+++ app.py
@@ -0,0 +1,63 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+from components import Column, Header, Row
+from auth import auth
+
+app = dash.Dash(
+    __name__
+)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Optionally display a log in screen.                                                                   #
+# If `REQUIRE_LOGIN = True` in `config.py`, then auth_instance allows you to programatically access the #
+# username of the currently logged in user.                                                             #
+# If `REQUIRE_LOGIN = False`, then no login screen will be displayed and `auth_instance` will be `None` #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+auth_instance = auth(app)
+
+server = app.server  # Expose the server variable for deployments
+
+# Standard Dash app code below
+app.layout = html.Div(className='container', children=[
+
+    Header('Sample App'),
+
+    Row([
+        Column(width=4, children=[
+            dcc.Dropdown(
+                id='dropdown',
+                options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+                value='LA'
+            )
+        ]),
+        Column(width=8, children=[
+            dcc.Graph(id='graph')
+        ])
+    ])
+])
+
+
+@app.callback(Output('graph', 'figure'),
+              [Input('dropdown', 'value')])
+def update_graph(value):
+    return {
+        'data': [{
+            'x': [1, 2, 3, 4, 5, 6],
+            'y': [3, 1, 2, 3, 5, 6]
+        }],
+        'layout': {
+            'title': value,
+            'margin': {
+                'l': 60,
+                'r': 10,
+                't': 40,
+                'b': 60
+            }
+        }
+    }
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
